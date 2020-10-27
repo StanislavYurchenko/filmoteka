c4b2f351916edb1dc3473ef32ab798555bee95a2
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-10-27 21:10:45 +0200

    created header

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,6 +1,12 @@
-import footerTemplate from '../template/footer.hbs'
+import footerTemplate from '../template/footer.hbs';
+import headerTemplate from '../template/header.hbs';
+// import './refs';
+// console.log(refs.header);
 console.log(footerTemplate());
 const footerRef = document.querySelector('#root-home-page');
+const headerRef = document.querySelector('#root-header');
 console.log(footerRef);
-
-footerRef.innerHTML = footerTemplate();
+console.log(headerRef);
+// footerRef.innerHTML = footerTemplate();
+headerRef.insertAdjacentHTML("afterbegin", headerTemplate())
+footerRef.insertAdjacentHTML("afterbegin", footerTemplate())
