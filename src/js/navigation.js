78c0bee36ae95e406eff2a9bf2ca8505b8f6b430
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-10-27 19:06:36 +0200

    start my task

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,1 +1,6 @@
-im
+import footerTemplate from '../template/footer.hbs'
+console.log(footerTemplate());
+const footerRef = document.querySelector('#root-home-page');
+console.log(footerRef);
+
+footerRef.innerHTML = footerTemplate();
