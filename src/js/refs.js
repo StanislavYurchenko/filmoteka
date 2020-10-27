cfb4c43ba6d0095257725589683984e57090038f
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-27 22:16:31 +0200

    fix refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,8 @@
-export default refs = {
+const refs = {
   header: document.querySelector('#root-header'),
   footer: document.querySelector('#root-footer'),
   homePage: document.querySelector('#root-home-page'),
   myFilmLibraryPage: document.querySelector('#root-my-film-library-page'),
   detailsPage: document.querySelector('#root-details-page'),
 }
+export default refs;
