be0edd7b16a52acef276c57b3ef7afb19a892948
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-28 11:48:49 +0200

    add data, fix html, install icon

---
+++ src/data/getPopular.js
@@ -0,0 +1,133 @@
+const getPopular = {
+    "page": 1,
+    "total_results": 10000,
+    "total_pages": 500,
+    "results": [
+        {
+            "popularity": 2627.575,
+            "vote_count": 23,
+            "video": false,
+            "poster_path": "/ugZW8ocsrfgI95pnQ7wrmKDxIe.jpg",
+            "id": 724989,
+            "adult": false,
+            "backdrop_path": "/86L8wqGMDbwURPni2t7FQ0nDjsH.jpg",
+            "original_language": "en",
+            "original_title": "Hard Kill",
+            "genre_ids": [
+                28,
+                53
+            ],
+            "title": "Hard Kill",
+            "vote_average": 4.2,
+            "overview": "The work of billionaire tech CEO Donovan Chalmers is so valuable that he hires mercenaries to protect it, and a terrorist group kidnaps his daughter just to get it.",
+            "release_date": "2020-10-23"
+        },
+        {
+            "popularity": 1993.358,
+            "vote_count": 193,
+            "video": false,
+            "poster_path": "/7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+            "id": 528085,
+            "adult": false,
+            "backdrop_path": "/5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+            "original_language": "en",
+            "original_title": "2067",
+            "genre_ids": [
+                18,
+                878,
+                53
+            ],
+            "title": "2067",
+            "vote_average": 5.5,
+            "overview": "A lowly utility worker is called to the future by a mysterious radio signal, he must leave his dying wife to embark on a journey that will force him to face his deepest fears in an attempt to change the fabric of reality and save humankind from its greatest environmental crisis yet.",
+            "release_date": "2020-10-01"
+        },
+        {
+            "popularity": 1447.764,
+            "vote_count": 18,
+            "video": false,
+            "poster_path": "/h8Rb9gBr48ODIwYUttZNYeMWeUU.jpg",
+            "id": 635302,
+            "adult": false,
+            "backdrop_path": "/xoqr4dMbRJnzuhsWDF3XNHQwJ9x.jpg",
+            "original_language": "ja",
+            "original_title": "劇場版「鬼滅の刃」無限列車編",
+            "genre_ids": [
+                28,
+                12,
+                16,
+                18,
+                14,
+                36
+            ],
+            "title": "Demon Slayer: Kimetsu no Yaiba - The Movie: Mugen Train",
+            "vote_average": 7.5,
+            "overview": "Tanjirō Kamado, joined with Inosuke Hashibira, a boy raised by boars who wears a boar's head, and Zenitsu Agatsuma, a scared boy who reveals his true power when he sleeps, boards the Infinity Train on a new mission with the Fire Hashira, Kyōjurō Rengoku, to defeat a demon who has been tormenting the people and killing the demon slayers who oppose it!",
+            "release_date": "2020-10-16"
+        },
+        {
+            "popularity": 1201.77,
+            "vote_count": 2277,
+            "video": false,
+            "poster_path": "/riYInlsq2kf1AWoGm80JQW5dLKp.jpg",
+            "id": 497582,
+            "adult": false,
+            "backdrop_path": "/kMe4TKMDNXTKptQPAdOF0oZHq3V.jpg",
+            "original_language": "en",
+            "original_title": "Enola Holmes",
+            "genre_ids": [
+                80,
+                18,
+                9648
+            ],
+            "title": "Enola Holmes",
+            "vote_average": 7.6,
+            "overview": "While searching for her missing mother, intrepid teen Enola Holmes uses her sleuthing skills to outsmart big brother Sherlock and help a runaway lord.",
+            "release_date": "2020-09-23"
+        },
+        {
+            "popularity": 1146.812,
+            "vote_count": 124,
+            "video": false,
+            "poster_path": "/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
+            "id": 741067,
+            "adult": false,
+            "backdrop_path": "/aO5ILS7qnqtFIprbJ40zla0jhpu.jpg",
+            "original_language": "en",
+            "original_title": "Welcome to Sudden Death",
+            "genre_ids": [
+                28,
+                12,
+                18,
+                53
+            ],
+            "title": "Welcome to Sudden Death",
+            "vote_average": 6.4,
+            "overview": "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
+            "release_date": "2020-09-29"
+        },
+        {
+            "popularity": 885.491,
+            "vote_count": 183,
+            "video": false,
+            "poster_path": "/r4Lm1XKP0VsTgHX4LG4syAwYA2I.jpg",
+            "id": 590223,
+            "adult": false,
+            "backdrop_path": "/lA5fOBqTOQBQ1s9lEYYPmNXoYLi.jpg",
+            "original_language": "en",
+            "original_title": "Love and Monsters",
+            "genre_ids": [
+                28,
+                12,
+                35,
+                878
+            ],
+            "title": "Love and Monsters",
+            "vote_average": 7.6,
+            "overview": "Seven years after the Monsterpocalypse, Joel Dawson, along with the rest of humanity, has been living underground ever since giant creatures took control of the land. After reconnecting over radio with his high school girlfriend Aimee, who is now 80 miles away at a coastal colony, Joel begins to fall for her again. As Joel realizes that there’s nothing left for him underground, he decides against all logic to venture out to Aimee, despite all the dangerous monsters that stand in his way.",
+            "release_date": "2020-10-16"
+        }        
+    ]
+};
+
+export default getPopular
