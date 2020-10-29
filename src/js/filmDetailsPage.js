5999ca0aa3a9c5a93ac9c3815666c1055c5b3a0d
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-10-29 19:05:08 +0300

    added details-page

--- src/js/filmDetailsPage.js
+++ src/js/filmDetailsPage.js
@@ -22,33 +22,43 @@ const monitorButtonStatusText = () => {
 
 const drawQueueFilmList = () => {
     const toQueueArray = [];
+    const localStorage = JSON.parse(localStorage.getItem(filmsQueue));
     const movie = localStorage.getItem(filmsQueue.find(movie => movie.original_title === selectFilm.original_title));
 
-    if(localStorage.getItem(filmsQueue) && movie && (movie !== selectFilm.original_title)) {
-        toQueueArray.push(movie);
+    if(localStorage) {
+        toWatchedArray.push(...localStorage);
+    };
+
+    if(localStorage && !movie) {
+        toQueueArray.push(selectFilm);
     } else {
         if(toQueueArray.length === 0) return;
         const indexOfTheMovieToBeDeleted = toQueueArray.indexOf(movie);
         toQueueArray.splice(indexOfTheMovieToBeDeleted, 1);
     };
 
-    localStorage.setItem(filmsQueue, toQueueArray);
+    localStorage.setItem(filmsQueue, JSON.stringify(toWatchedArray));
     monitorButtonStatusText();
 };
 
 const drawWatchedFilmList = () => {
     const toWatchedArray = [];
+    const localStorage = JSON.parse(localStorage.getItem(filmsWatched));
     const movie = localStorage.getItem(filmsWatched.find(movie => movie.original_title === selectFilm.original_title));
+    
+    if(localStorage) {
+        toWatchedArray.push(...localStorage);
+    };
 
-    if(localStorage.getItem(filmsWatched) && movie && (movie !== selectFilm.original_title)) {
-        toWatchedArray.push(movie);
+    if(localStorage && !movie) {
+        toWatchedArray.push(selectFilm);
     } else {
         if(toWatchedArray.length === 0) return;
         const indexOfTheMovieToBeDeleted = toWatchedArray.indexOf(movie);
         toWatchedArray.splice(indexOfTheMovieToBeDeleted, 1);
     };
 
-    localStorage.setItem(filmsWatched, toWatchedArray);
+    localStorage.setItem(filmsWatched, JSON.stringify(toWatchedArray));
     monitorButtonStatusText();
 };
 
