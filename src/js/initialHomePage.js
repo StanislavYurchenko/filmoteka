e3f7068e6f893c7ecd5b97804b10ab3f331c3483
ArturStahov <vtemy.at.ua@gmail.com>
2020-10-29 19:44:16 +0200

    fix2000

--- src/js/initialHomePage.js
+++ src/js/initialHomePage.js
@@ -1,23 +1,13 @@
-// import './searchAndPaginationHomePage.js';
-// import updateHomeMarkup from './searchAndPaginationHomePage';
-
-// updateHomeMarkup();
-// import getDetails from '../data/getDetails';
-// import getPopular from '../data/getPopular';
-
-// console.log(getDetails);
-// console.log(getPopular);
-
-import FilmListTemplate from '../template/homePage.hbs';
+import filmListTemplate from '../template/homePageContent.hbs';
 import refs from './refs.js';
 const apiKey = '81f248d3c9154788229a5419bb33091a';
 const baseUrl = 'https://api.themoviedb.org';
 
 let pageNumber = 1;
 
-
-const renderFilmList = (template, arrFilms, renderRefSelector) => {
-  renderRefSelector.innerHTML = template(arrFilms);
+const renderFilmList = (template, arrFilms) => {
+  const ref = refs.homePage.querySelector('form')
+  ref.insertAdjacentHTML('afterend', filmListTemplate(arrFilms))
 
 };
 
@@ -28,35 +18,18 @@ const fetchPopularMoviesList = (baseUrl, pageNumber, apiKey) => {
 };
 
 
-const formatDataNullImages = arrData => {
+const formatDataNullImages = (arrData) => {
   const formatDate = arrData.map(el => {
-    if (typeof el.backdrop_path === 'object') {
+    if (typeof el.backdrop_path === "object") {
       el.backdrop_path = `./images/temp.png`;
     } else {
-      el.backdrop_path = `https://image.tmdb.org/t/p/w500/${el.backdrop_path}`;
+      el.backdrop_path = `https://image.tmdb.org/t/p/w500/${el.backdrop_path}`
     }
-    return el;
-  });
+    return el
+  })
   return formatDate;
-};
+}
 
-fetchPopularMoviesList(baseUrl, pageNumber, apiKey).then(data => {
-  const arrData = data.results;
 
-  renderFilmList(
-    FilmListTemplate,
-    formatDataNullImages(arrData),
-    refs.homePage,
-  );
-  // console.log(arrData)
-  // renderFilmList(FilmListTemplate, arrData, refs.homePage);
-});
 
-export default {
-  fetchPopularMoviesList,
-  pageNumber,
-  apiKey,
-  renderFilmList,
-  baseUrl,
-  formatDataNullImages,
-};
+export { renderFilmList, fetchPopularMoviesList, baseUrl, apiKey, pageNumber };
