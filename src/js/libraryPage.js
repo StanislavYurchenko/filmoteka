f7997ee851a576cb4e89ae163c82b03b9aba59f5
vovababich <vova_babich@list.ru>
2020-10-29 03:18:11 +0200

    added export {drawQueueFilmList, drawWatchedFilmList}

--- src/js/libraryPage.js
+++ src/js/libraryPage.js
@@ -2,15 +2,15 @@ import myFilmLibraryPage from '../template/myFilmLibraryPage.hbs';
 import refs from './refs';
 import { notice } from './pnotify';
 
-// fetch(
-//   'https://api.themoviedb.org/3/search/movie?api_key=81f248d3c9154788229a5419bb33091a&language=en-US&query=bad&page=1&include_adult=false',
-// )
-//   .then(res => res.json())
-//   .then(movies => {
-//     const { results } = movies;
-//     localStorage.setItem('filmsQueue', JSON.stringify(results));
-//     localStorage.setItem('filmsWatched', JSON.stringify(results));
-//   });
+fetch(
+  'https://api.themoviedb.org/3/search/movie?api_key=81f248d3c9154788229a5419bb33091a&language=en-US&query=bad&page=1&include_adult=false',
+)
+  .then(res => res.json())
+  .then(movies => {
+    const { results } = movies;
+    localStorage.setItem('filmsQueue', JSON.stringify(results));
+    localStorage.setItem('filmsWatched', JSON.stringify(results));
+  });
 
 function createLibraryCardFunc(parsedLocalStorage, message) {
   if (!parsedLocalStorage) {
@@ -38,5 +38,6 @@ function drawWatchedFilmList() {
   createLibraryCardFunc(parsedLocalStorage, message);
 }
 
+export { drawQueueFilmList, drawWatchedFilmList };
 // drawQueueFilmList();
 // drawWatchedFilmList();
