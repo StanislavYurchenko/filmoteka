36ca557ae63802a88dab1fbde9ad5a051f56408d
vovababich <vova_babich@list.ru>
2020-10-30 02:30:57 +0200

    added library buttons

--- src/js/filmDetailsPage.js
+++ src/js/filmDetailsPage.js
@@ -3,78 +3,83 @@ import detailsFilms from '../template/detailsPage.hbs';
 import 'material-design-icons/iconfont/material-icons.css';
 import refs from './refs';
 
-
 const monitorButtonStatusText = () => {
-// const filmsQueueInLocalStorage = JSON.parse(localStorage.getItem('filmsQueue')).find(movie => movie.original_title === selectFilm.original_title);
-// const filmsWatchedInLocalStorage = JSON.parse(localStorage.getItem('filmsWatched')).find(movie => movie.original_title === selectFilm.original_title);
+  // const filmsQueueInLocalStorage = JSON.parse(localStorage.getItem('filmsQueue')).find(movie => movie.original_title === selectFilm.original_title);
+  // const filmsWatchedInLocalStorage = JSON.parse(localStorage.getItem('filmsWatched')).find(movie => movie.original_title === selectFilm.original_title);
 
-const buttonWatched = document.querySelector('.details__button-watched');
-const buttonQueue = document.querySelector('.details__button-queue');
+  const buttonWatched = document.querySelector('.details__button-watched');
+  const buttonQueue = document.querySelector('.details__button-queue');
 
+  const filmsQueueInLocalStorage = JSON.parse(
+    localStorage.getItem('filmsQueue'),
+  ).find(movie => movie.original_title === getDetails.original_title);
+  const filmsWatchedInLocalStorage = JSON.parse(
+    localStorage.getItem('filmsWatched'),
+  ).find(movie => movie.original_title === getDetails.original_title);
 
-const filmsQueueInLocalStorage = JSON.parse(localStorage.getItem('filmsQueue')).find(movie => movie.original_title === getDetails.original_title);
-const filmsWatchedInLocalStorage = JSON.parse(localStorage.getItem('filmsWatched')).find(movie => movie.original_title === getDetails.original_title);
+  if (filmsQueueInLocalStorage === getDetails.original_title) {
+    buttonQueue.innerHTML = `<i class="material-icons details__icons">event_busy</i> Delete from queue`;
+  } else {
+    buttonQueue.innerHTML = `<i class="material-icons details__icons">event_busy</i> Add to queue`;
+  }
 
-    if(filmsQueueInLocalStorage === getDetails.original_title) {
-        buttonQueue.innerHTML = `<i class="material-icons details__icons">event_busy</i> Delete from queue`;
-    } else {
-        buttonQueue.innerHTML = `<i class="material-icons details__icons">event_busy</i> Add to queue`;
-    };
-
-    if(filmsWatchedInLocalStorage === getDetails.original_title) {
-        buttonWatched.innerHTML = `<i class="material-icons details__icons">videocam</i> Delete from watched`;
-    } else {
-        buttonWatched.innerHTML = `<i class="material-icons details__icons">videocam</i> Add to watched`;
-    };
-
+  if (filmsWatchedInLocalStorage === getDetails.original_title) {
+    buttonWatched.innerHTML = `<i class="material-icons details__icons">videocam</i> Delete from watched`;
+  } else {
+    buttonWatched.innerHTML = `<i class="material-icons details__icons">videocam</i> Add to watched`;
+  }
 };
 
 const drawQueueFilmList = () => {
-    const toQueueArray = [];
-    const localStorage = JSON.parse(localStorage.getItem('filmsQueue'));
-    const movie = localStorage.getItem('filmsQueue').find(movie => movie.original_title === selectFilm.original_title);
+  const toQueueArray = [];
+  const localStorage = JSON.parse(localStorage.getItem('filmsQueue'));
+  const movie = localStorage
+    .getItem('filmsQueue')
+    .find(movie => movie.original_title === selectFilm.original_title);
 
-    if(localStorage) {
-        toWatchedArray.push(...localStorage);
-    };
+  if (localStorage) {
+    toWatchedArray.push(...localStorage);
+  }
 
-    if(localStorage && !movie) {
-        toQueueArray.push(selectFilm);
-    } else {
-        if(toQueueArray.length === 0) return;
-        const indexOfTheMovieToBeDeleted = toQueueArray.indexOf(movie);
-        toQueueArray.splice(indexOfTheMovieToBeDeleted, 1);
-    };
+  if (localStorage && !movie) {
+    toQueueArray.push(selectFilm);
+  } else {
+    if (toQueueArray.length === 0) return;
+    const indexOfTheMovieToBeDeleted = toQueueArray.indexOf(movie);
+    toQueueArray.splice(indexOfTheMovieToBeDeleted, 1);
+  }
 
-    localStorage.setItem('filmsQueue', JSON.stringify(toQueueArray));
-    monitorButtonStatusText();
+  localStorage.setItem('filmsQueue', JSON.stringify(toQueueArray));
+  monitorButtonStatusText();
 };
 
 const drawWatchedFilmList = () => {
-    const toWatchedArray = [];
-    const localStorage = JSON.parse(localStorage.getItem('filmsWatched'));
-    const movie = localStorage.getItem('filmsWatched').find(movie => movie.original_title === selectFilm.original_title);
-    
-    if(localStorage) {
-        toWatchedArray.push(...localStorage);
-    };
+  const toWatchedArray = [];
+  const localStorage = JSON.parse(localStorage.getItem('filmsWatched'));
+  const movie = localStorage
+    .getItem('filmsWatched')
+    .find(movie => movie.original_title === selectFilm.original_title);
 
-    if(localStorage && !movie) {
-        toWatchedArray.push(selectFilm);
-    } else {
-        if(toWatchedArray.length === 0) return;
-        const indexOfTheMovieToBeDeleted = toWatchedArray.indexOf(movie);
-        toWatchedArray.splice(indexOfTheMovieToBeDeleted, 1);
-    };
+  if (localStorage) {
+    toWatchedArray.push(...localStorage);
+  }
 
-    localStorage.setItem('filmsWatched', JSON.stringify(toWatchedArray));
-    monitorButtonStatusText();
+  if (localStorage && !movie) {
+    toWatchedArray.push(selectFilm);
+  } else {
+    if (toWatchedArray.length === 0) return;
+    const indexOfTheMovieToBeDeleted = toWatchedArray.indexOf(movie);
+    toWatchedArray.splice(indexOfTheMovieToBeDeleted, 1);
+  }
+
+  localStorage.setItem('filmsWatched', JSON.stringify(toWatchedArray));
+  monitorButtonStatusText();
 };
 
-const showDetails = (selectFilm) => {
-    const temp = detailsFilms(getDetails);
-    refs.detailsPage.innerHTML = temp;
-    monitorButtonStatusText();
+const showDetails = selectFilm => {
+  const temp = detailsFilms(getDetails);
+  refs.detailsPage.innerHTML = temp;
+  monitorButtonStatusText();
 };
 
 export { showDetails, drawQueueFilmList, drawWatchedFilmList };
