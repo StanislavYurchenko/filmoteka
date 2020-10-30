3afccc1c5929b3bd4b116f280e05098d1ee12552
ArturStahov <vtemy.at.ua@gmail.com>
2020-10-30 14:09:21 +0200

    add function formattingFethData

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/main.scss';
 import './js/navigation';
 import img from './images/temp.png';
 import refs from './js/refs.js';
-import { renderFilmList, fetchPopularMoviesList, baseUrl, apiKey, pageNumber } from './js/initialHomePage';
+import { renderFilmList, fetchPopularMoviesList, baseUrl, apiKey, pageNumber, formattingFethData } from './js/initialHomePage';
 import { usersSearch, renderForm, renderNavigate } from './js/searchAndPaginationHomePage';
 import formTemplate from './template/homePageForm.hbs';
 import filmListTemplate from './template/homePageContent.hbs';
@@ -15,10 +15,11 @@ renderForm(formTemplate);
 //render navigations
 renderNavigate(navigateTemplate);
 
+
 // render movies
 fetchPopularMoviesList(baseUrl, pageNumber, apiKey).then(data => {
     const arrData = data.results;
-    renderFilmList(filmListTemplate, arrData);
+    renderFilmList(filmListTemplate, formattingFethData(arrData));
 });
 
 //render query
