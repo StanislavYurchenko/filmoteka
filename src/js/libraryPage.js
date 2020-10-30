e60f7abf54a843700897e537945d7765007b0272
vovababich <vova_babich@list.ru>
2020-10-30 02:45:23 +0200

    saved

--- src/js/libraryPage.js
+++ src/js/libraryPage.js
@@ -10,25 +10,25 @@ const queueBtn = document.querySelector('.queue');
 watchedBtn.addEventListener('click', drawWatchedFilmList);
 queueBtn.addEventListener('click', drawQueueFilmList);
 
-// fetch(
-//   'https://api.themoviedb.org/3/search/movie?api_key=81f248d3c9154788229a5419bb33091a&language=en-US&query=bad&page=1&include_adult=false',
-// )
-//   .then(res => res.json())
-//   .then(movies => {
-//     const { results } = movies;
-//     localStorage.setItem('filmsWatched', JSON.stringify(results));
-//     // localStorage.setItem('filmsQueue', JSON.stringify(results));
-//   });
+fetch(
+  'https://api.themoviedb.org/3/search/movie?api_key=81f248d3c9154788229a5419bb33091a&language=en-US&query=bad&page=1&include_adult=false',
+)
+  .then(res => res.json())
+  .then(movies => {
+    const { results } = movies;
+    localStorage.setItem('filmsWatched', JSON.stringify(results));
+    // localStorage.setItem('filmsQueue', JSON.stringify(results));
+  });
 
-// fetch(
-//   'https://api.themoviedb.org/3/search/movie?api_key=81f248d3c9154788229a5419bb33091a&language=en-US&query=bad&page=2&include_adult=false',
-// )
-//   .then(res => res.json())
-//   .then(movies => {
-//   const { results } = movies;
-//   // localStorage.setItem('filmsWatched', JSON.stringify(results));
-//   localStorage.setItem('filmsQueue', JSON.stringify(results));
-// });
+fetch(
+  'https://api.themoviedb.org/3/search/movie?api_key=81f248d3c9154788229a5419bb33091a&language=en-US&query=bad&page=2&include_adult=false',
+)
+  .then(res => res.json())
+  .then(movies => {
+    const { results } = movies;
+    // localStorage.setItem('filmsWatched', JSON.stringify(results));
+    localStorage.setItem('filmsQueue', JSON.stringify(results));
+  });
 
 function createLibraryCardFunc(parsedLocalStorage, message) {
   if (!parsedLocalStorage) {
