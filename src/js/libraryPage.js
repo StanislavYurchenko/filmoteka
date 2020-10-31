ee007247ed9725d94e2ca3c008d704434a7b7deb
vovababich <vova_babich@list.ru>
2020-10-31 02:16:49 +0200

    saved

--- src/js/libraryPage.js
+++ src/js/libraryPage.js
@@ -4,24 +4,6 @@ import { activeDetailsPage } from './navigation';
 import refs from './refs';
 import { notice } from './pnotify';
 
-fetch(
-  'https://api.themoviedb.org/3/search/movie?api_key=81f248d3c9154788229a5419bb33091a&language=en-US&query=bad&page=1&include_adult=false',
-)
-  .then(res => res.json())
-  .then(movies => {
-    const { results } = movies;
-    localStorage.setItem('filmsWatched', JSON.stringify(results));
-  });
-
-fetch(
-  'https://api.themoviedb.org/3/search/movie?api_key=81f248d3c9154788229a5419bb33091a&language=en-US&query=bad&page=2&include_adult=false',
-)
-  .then(res => res.json())
-  .then(movies => {
-    const { results } = movies;
-    localStorage.setItem('filmsQueue', JSON.stringify(results));
-  });
-
 function renderLibraryButtons(template) {
   refs.myFilmLibraryPage.insertAdjacentHTML('afterbegin', template());
 }
@@ -35,41 +17,42 @@ function serviceLibraryButtons(template) {
 }
 
 function createLibraryCardFunc(parsedLocalStorage, message) {
-  if (!parsedLocalStorage) {
+  if (!parsedLocalStorage || !parsedLocalStorage.length) {
     notice(message);
     return;
   }
-  refs.libraryList.innerHTML = '';
+  // refs.libraryList.innerHTML = '';
   const formatData = formattingFethData(parsedLocalStorage);
-  const fragment = myFilmLibraryPage(formatData);
-  refs.libraryList.insertAdjacentHTML('beforeend', fragment);
+  const renderLibraryList = myFilmLibraryPage(formatData);
+  refs.libraryList.insertAdjacentHTML('beforeend', renderLibraryList);
 
   refs.libraryList.removeEventListener('click', onClickFilmAtMyLibrary);
   refs.libraryList.addEventListener('click', onClickFilmAtMyLibrary);
-}
 
-function onClickFilmAtMyLibrary(e) {
-  if (e.target.nodeName !== 'LI') {
-    return;
-  }
+  function onClickFilmAtMyLibrary(e) {
+    console.log(e.target.dataset.itemid);
+    if (e.target.nodeName !== 'LI') {
+      return;
+    }
 
-  refs.queueBtn.classList.contains('library-btn--active')
-    ? renderDetailPageFromLibrary('filmsQueue')
-    : renderDetailPageFromLibrary('filmsWatched');
+    refs.queueBtn.classList.contains('library-btn--active')
+      ? renderDetailPageFromLibrary('filmsQueue')
+      : renderDetailPageFromLibrary('filmsWatched');
 
-  function renderDetailPageFromLibrary(query) {
-    const arr = JSON.parse(localStorage.getItem(query));
-    let detailFilm = arr.find(
-      filmData => filmData.id === Number(e.target.dataset.itemid),
-    );
-    activeDetailsPage(detailFilm);
+    function renderDetailPageFromLibrary(query) {
+      const arr = JSON.parse(localStorage.getItem(query));
+      let detailFilm = arr.find(
+        filmData => filmData.id === Number(e.target.dataset.itemid),
+      );
+      activeDetailsPage(detailFilm);
+    }
   }
 }
 
 function drawQueueFilmList() {
   refs.watchedBtn.classList.remove('library-btn--active');
   refs.queueBtn.classList.add('library-btn--active');
-
+  refs.libraryList.innerHTML = '';
   const message = 'You do not have to queue movies to watch. Add them.';
   let readLocalStorage = localStorage.getItem('filmsQueue');
   const parsedLocalStorage = JSON.parse(readLocalStorage);
@@ -80,7 +63,7 @@ function drawQueueFilmList() {
 function drawWatchedFilmList() {
   refs.queueBtn.classList.remove('library-btn--active');
   refs.watchedBtn.classList.add('library-btn--active');
-
+  refs.libraryList.innerHTML = '';
   const message = 'You do not have watched movies. Add them';
   let readLocalStorage = localStorage.getItem('filmsWatched');
   const parsedLocalStorage = JSON.parse(readLocalStorage);
