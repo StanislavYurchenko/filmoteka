2cd0aa67dff0a8e113c88a0a2edf3c566c72e1ca
vovababich <vova_babich@list.ru>
2020-10-31 02:23:51 +0200

    prettier in index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import './sass/main.scss';
 import './js/navigation';
 import img from './images/temp.png';
 import refs from './js/refs.js';
-import {
-  renderFilmList,
-  fetchPopularMoviesList,
-  baseUrl,
-  apiKey,
-  pageNumber,
-  formattingFethData,
-} from './js/initialHomePage';
-import {
-  usersSearch,
-  renderForm,
-  renderNavigate,
-} from './js/searchAndPaginationHomePage';
+import { renderFilmList, fetchPopularMoviesList, baseUrl, apiKey, pageNumber, formattingFethData } from './js/initialHomePage';
+import { usersSearch, renderForm, renderNavigate } from './js/searchAndPaginationHomePage';
 import { serviceLibraryButtons } from './js/libraryPage';
 import formTemplate from './template/homePageForm.hbs';
 import filmListTemplate from './template/homePageContent.hbs';
