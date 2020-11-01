2c8848c8ecb05b614e4e4d3f07f0cc81587b039b
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2020-11-01 14:32:58 +0200

    added input debounce

--- src/js/searchAndPaginationHomePage.js
+++ src/js/searchAndPaginationHomePage.js
@@ -2,19 +2,22 @@ import refs from './refs';
 import homePageTpl from '../template/homePageContent.hbs';
 import 'material-design-icons/iconfont/material-icons.css';
 import { formattingFetchData, fetchPopularMoviesList, baseUrl } from './initialHomePage';
+import { debounce } from "debounce";
+
 const apiKey = '81f248d3c9154788229a5419bb33091a';
 let formRef = null;
 let btn_next = null;
 let btn_prev = null;
 let page_span = null;
-let requir = null;
+let require = null;
+let inputRef = null;
 
 const controlGlobalPage = {
   isStartGlobalPage: true,
   setStartPage() {
     this.isStartGlobalPage = true;
-    if (requir !== null) {
-      requir.classList.add('is-hidden');
+    if (require !== null) {
+      require.classList.add('is-hidden');
       formRef.reset();
     }
 
@@ -32,9 +35,13 @@ const renderNavigate = template => {
   refs.homePage.insertAdjacentHTML('beforeend', template());
 };
 
+const debouncedSearchFilmsHandler =  debounce(event => searchFilmsHandler(event), 1000);
+
 function usersSearch() {
   formRef = refs.homePage.querySelector('form');
   formRef.addEventListener('submit', searchFilmsHandler);
+  inputRef = formRef.elements.query;
+  inputRef.addEventListener('input', debouncedSearchFilmsHandler);
   btn_next = refs.homePage.querySelector('.button-next');
   btn_prev = refs.homePage.querySelector('.button-prev');
   page_span = refs.homePage.querySelector('.page');
@@ -44,6 +51,8 @@ function usersSearch() {
     btn_prev.setAttribute('disabled', 'disabled');
   }
 }
+
+
 
 function homePagePagination() {
   fetchPopularMoviesList(baseUrl, films.pageNumb, apiKey).then(data => {
@@ -87,6 +96,7 @@ function handlerPrev() {
 }
 
 function searchFilmsHandler(event) {
+  console.log('event',event);
   event.preventDefault();
   films.resetPage();
   const formData = new FormData(formRef);
@@ -111,12 +121,12 @@ function changeButtonPagActive(data) {
 
 function fetchMovies() {
   films.fetchFilms().then(data => {
-    requir = refs.homePage.querySelector('.form-search__requirements');
+    require = refs.homePage.querySelector('.form-search__requirements');
     if (data.length === 0) {
-      requir.classList.remove('is-hidden');
+      require.classList.remove('is-hidden');
       btn_next.setAttribute('disabled', 'disabled');
     } else {
-      requir.classList.add('is-hidden');
+      require.classList.add('is-hidden');
     }
     if (films.inputValue === ' ') return;
     const markup = data.length === 0 ? '' : homePageTpl(formattingFetchData(data));
@@ -135,7 +145,7 @@ const films = {
         `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${this.inputValue}&page=${this.pageNumb}&include_adult=false`,
       );
       const data = await response.json();
-      console.log('data', data);
+      // console.log('data', data);
       changeButtonPagActive(data);
       return data.results;
     } catch (error) {
