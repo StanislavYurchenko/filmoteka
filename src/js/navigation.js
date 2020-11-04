8936806ebc618b4814954159aadd1ba0569ce464
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2020-11-04 20:19:40 +0200

    hide button library

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -105,10 +105,12 @@ function changeLoginBtnStatus(isLogged) {
     textRef.textContent = 'Exit';
     imgRef.src = '../images/user_form/exit.png';
     imgRef.alt = 'exit icon';
+    refs.linkMyLibrary.classList.remove('visually-hidden');
   } else {
     textRef.textContent = 'Login';
     imgRef.src = '../images/user_form/enter.png';
     imgRef.alt = 'login icon';
+    refs.linkMyLibrary.classList.add('visually-hidden');
   }
 }
 
